abc88607f2e64625315e546647905aa7cb7176a3
activiteOCR <julien.terrier3@wanadoo.fr>
2022-11-12 17:25:05 +0100

    placeholder modification

--- source/discord.ts
+++ source/discord.ts
@@ -1,105 +1,116 @@
-import { log } from 'console'
-import { channel } from 'diagnostics_channel'
-import DiscordJS, { ChannelManager, Intents } from 'discord.js'
-import dotenv from 'dotenv'
-dotenv.config() // to get access to the variables from .env file
+import { log } from "console";
+import { channel } from "diagnostics_channel";
+import DiscordJS, { ChannelManager, Intents } from "discord.js";
+import dotenv from "dotenv";
+dotenv.config(); // to get access to the variables from .env file
 
-const GUILD_ID = "1013893737186869318"
-const CHANNEL_ID = "1013895771294277632"
+const GUILD_ID = "1013893737186869318";
+const CHANNEL_ID = "1013895771294277632";
 
 const client = new DiscordJS.Client({
-    intents: [ //specify the intentiens of our bot, what he want to do in discord
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MEMBERS
-    ]
-})
+  intents: [
+    //specify the intentiens of our bot, what he want to do in discord
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MEMBERS,
+  ],
+});
 console.log("#################discord");
 
 let guild: any = null;
 
-client.on('ready', () => {
-    console.log('#Discord : bot ready', client.user?.tag);
-    guild = client.guilds.cache.get(GUILD_ID)
-    if (guild !== null) {
-        console.log('#Discord :guild available');
-        guild?.members.fetch()
-            .then((data: any) => {
-                data.forEach((element: any) => {
-                    console.log(`user tag: ${element.user.tag}\tisBot: ${element.user.bot}`);
-
-                });
-            }).catch((err: any) => {
-                console.log('#discord error ', err)
-            })
-    }
-})
-//Start discord BOT 
-client.login(process.env.DISCORD_TOKEN).then(() => {
-    console.log('#discord : connected');
-}).catch((err) => {
-    console.error('#discord error: ', err);
-})
+client.on("ready", () => {
+  console.log("#Discord : bot ready", client.user?.tag);
+  guild = client.guilds.cache.get(GUILD_ID);
+  if (guild !== null) {
+    console.log("#Discord :guild available");
+    guild?.members
+      .fetch()
+      .then((data: any) => {
+        data.forEach((element: any) => {
+          console.log(
+            `user tag: ${element.user.tag}\tisBot: ${element.user.bot}`
+          );
+        });
+      })
+      .catch((err: any) => {
+        console.log("#discord error ", err);
+      });
+  }
+});
+//Start discord BOT
+client
+  .login(process.env.DISCORD_TOKEN)
+  .then(() => {
+    console.log("#discord : connected");
+  })
+  .catch((err) => {
+    console.error("#discord error: ", err);
+  });
 
 export const verifyUser = (user: string) => {
-    return new Promise<any>((resolve, reject) => {
-        // console.log("user : ", JSON.stringify(user));
-        guild?.members.fetch()
-            .then((data: any) => {
-                console.log("# Discord promise resolved");
-                let status: boolean = false;
-                data.forEach((element: any) => {
-                    if (element.user.tag === user) status = true
-                });
-                resolve(status);
-            }).catch((err: any) => {
-                console.log('error ', err)
-                reject(`#discord error : ${err}`)
-            })
-    })
-}
+  return new Promise<any>((resolve, reject) => {
+    // console.log("user : ", JSON.stringify(user));
+    guild?.members
+      .fetch()
+      .then((data: any) => {
+        console.log("# Discord promise resolved");
+        let status: boolean = false;
+        data.forEach((element: any) => {
+          if (element.user.tag === user) status = true;
+        });
+        resolve(status);
+      })
+      .catch((err: any) => {
+        console.log("error ", err);
+        reject(`#discord error : ${err}`);
+      });
+  });
+};
 
 export const sendMessageParticipate = async (user: string) => {
-    new Promise<void>( async(resolve, reject) => {
-        try {
-            let channel: any = client.channels.cache.get(CHANNEL_ID);
-            let discordUser: any = await getMemberByTag(user)
-            console.log("user ", discordUser)
-            const attachment = new DiscordJS.MessageAttachment('./source/assets/images/cryptoRocket.png')
-            const embed = new DiscordJS.MessageEmbed()
-                .setDescription(`See you for the raffle TBD <@${discordUser.id}> !`)
-                .setTitle(`Good luck getting on the Crypto raffle Meg4mint 👊`)
-                .setColor(0xffdd00)
-                // .setImage(discordUser.displayAvatarURL({ format: 'png' })) //display user Avatar
-                .setImage('attachment://cryptoRocket.png')
-            // channel.send(`<@${discordUser.id}> `)
-            await channel.send({ embeds: [embed], files:[attachment] })
-            resolve()
-        } catch (error) {
-            console.log(error);
-            reject(error)
-        }
-    })
-    //discord js : send a message to the DISCORD_CHANEL
-
-}
+  new Promise<void>(async (resolve, reject) => {
+    try {
+      let channel: any = client.channels.cache.get(CHANNEL_ID);
+      let discordUser: any = await getMemberByTag(user);
+      console.log("user ", discordUser);
+      const attachment = new DiscordJS.MessageAttachment(
+        "./source/assets/images/legend.jpg"
+      );
+      const embed = new DiscordJS.MessageEmbed()
+        .setDescription(`See you for the raffle <@${discordUser.id}> !`)
+        .setTitle(`Good luck getting on the Legendary raffle of Meg4mint 👊`)
+        .setColor(0xffdd00)
+        // .setImage(discordUser.displayAvatarURL({ format: 'png' })) //display user Avatar
+        .setImage("attachment://legend.jpg");
+      // channel.send(`<@${discordUser.id}> `)
+      await channel.send({ embeds: [embed], files: [attachment] });
+      resolve();
+    } catch (error) {
+      console.log(error);
+      reject(error);
+    }
+  });
+  //discord js : send a message to the DISCORD_CHANEL
+};
 
 export const getMemberByTag = async (member: string) => {
-    return new Promise<void>((resolve, reject) => {
-        guild?.members.fetch()
-            .then((data: any) => {
-                // console.log("in member Get");
-                //element is a GuildMember
-                data.forEach((element: any) => {
-                    if (element.user.tag === member) {
-                        resolve(element.user)
-                    }
-                });
-                reject({message:"invalid discord member"})
-            }).catch((err: any) => {
-                console.log('error ', err)
-                reject(err)
-            })
-    })
-
-}
+  return new Promise<void>((resolve, reject) => {
+    guild?.members
+      .fetch()
+      .then((data: any) => {
+        // console.log("in member Get");
+        //element is a GuildMember
+        data.forEach((element: any) => {
+          if (element.user.tag === member) {
+            resolve(element.user);
+          }
+        });
+        reject({ message: "invalid discord member" });
+      })
+      .catch((err: any) => {
+        console.log("error ", err);
+        reject(err);
+      });
+  });
+};
